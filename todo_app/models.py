47754bb663ebda18152261baf0932bd2b3222445
jpchato <jpchato@gmail.com>
2022-09-28 16:35:30 -0700

    made migrations, created superuser, added gitignore

--- todo_app/models.py
+++ todo_app/models.py
@@ -4,16 +4,17 @@ from django.utils import timezone
 from django.db import models
 from django.urls import reverse
 
-# Create your models here.
-
 def one_week_hence():
     return timezone.now() + timezone.timedelta(days=7)
 
 class ToDoList(models.Model):
     title = models.CharField(max_length=100, unique=True)
-    
+
     def get_absolute_url(self):
         return reverse("list", args=[self.id])
+
+    def __str__(self):
+        return self.title
 
 class ToDoItem(models.Model):
     title = models.CharField(max_length=100)
@@ -31,4 +32,4 @@ class ToDoItem(models.Model):
         return f"{self.title}: due {self.due_date}"
 
     class Meta:
-        ordering = ['due date']
+        ordering = ["due_date"]
